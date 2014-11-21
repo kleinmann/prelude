926e89ea5d0b2b447e0f52606e829ee01cc2818e
Uwe Kleinmann <u.kleinmann@googlemail.com>
2014-11-21 17:35:48 +0100

    Add evil-mode and configuration.

--- personal/00-packages.el
+++ personal/00-packages.el
@@ -1,1 +1,1 @@
-(prelude-ensure-module-deps '(eproject etags-table evil powerline rspec-mode smex solarized-theme web-mode yasnippet))
+(prelude-ensure-module-deps '(eproject etags-table evil evil-leader evil-matchit evil-nerd-commenter powerline rspec-mode smex solarized-theme web-mode yasnippet))
--- personal/z-evil.el
+++ personal/z-evil.el
@@ -1,1 +1,10 @@
+(global-evil-matchit-mode 1)
+
+(global-evil-leader-mode)
+(evil-leader/set-leader ",")
+
 (evil-mode)
+
+(evil-leader/set-key
+ "#" 'evilnc-comment-or-uncomment-lines
+)
