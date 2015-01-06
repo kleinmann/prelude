c58dcbd5ed905b277e0390e1b5cd9f066d92f66b
Uwe Kleinmann <u.kleinmann@googlemail.com>
2015-01-06 19:58:31 +0100

    Add more package repositories and restclient.

--- personal/00-packages.el
+++ personal/00-packages.el
@@ -1,1 +1,12 @@
-(prelude-ensure-module-deps '(eproject etags-table evil evil-leader evil-matchit evil-nerd-commenter powerline rspec-mode smex solarized-theme web-mode yasnippet))
+(add-to-list 'package-archives
+             '("MELPA" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("orgmode" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives
+             '("ELPA" . "http://tromey.com/elpa/") t)
+(add-to-list 'package-archives
+             '("gnu" . "http://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+(prelude-ensure-module-deps '(eproject etags-table evil evil-leader evil-matchit evil-nerd-commenter powerline restclient rspec-mode smex solarized-theme web-mode yasnippet))
